3cd5be558556110fe50730ac9cf4d49b52d6a6a6
Patrick Flynn <pflynn157@gmail.com>
2023-10-22 18:02:33 -0400

    Removed build files

--- build/file-explorer/src/CMakeFiles/CppExplorer.dir/CppExplorer_autogen/EWIEGA46WW/qrc_rsc.cpp.o.d
+++
@@ -1,3 +0,0 @@
-file-explorer/src/CMakeFiles/CppExplorer.dir/CppExplorer_autogen/EWIEGA46WW/qrc_rsc.cpp.o: \
- /home/patrick/Projects/sr.ht/micro-desktop/build/file-explorer/src/CppExplorer_autogen/EWIEGA46WW/qrc_rsc.cpp \
- /usr/include/stdc-predef.h
--- build/file-explorer/src/CMakeFiles/CppExplorer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CppExplorer.
-# This may be replaced when dependencies are built.
